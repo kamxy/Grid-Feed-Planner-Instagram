54c96da6ef5563763b93c82a70fae27e65dee367
Mehmet Kamay <mehmetkamay506@gmail.com>
2024-12-30 12:03:36 +0300

    last changes

--- InstagramGridPreview/InstagramGridPreviewApp.swift
+++ InstagramGridPreview/InstagramGridPreviewApp.swift
@@ -10,11 +10,16 @@ import SwiftUI
 @main
 struct InstagramGridPreviewApp: App {
     let persistenceController = PersistenceController.shared
+    @StateObject var viewModel = OnboardingViewModel()
 
     var body: some Scene {
         WindowGroup {
-            HomeView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            if viewModel.isOnboardingDone {
+                HomeView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            } else {
+                OnboardingView(viewModel: viewModel)
+            }
         }
     }
 }
